7a2ee8fc54594859c872d933d79b794903095942
Hungtr-uofa <hungtr@email.arizona.edu>
2021-12-08 02:22:47 -0700

    added escape str for delete

--- sql/design_delete.sql
+++ sql/design_delete.sql
@@ -2,7 +2,7 @@ BEGIN
   FOR rec IN
     (
         SELECT table_name FROM all_tables
-        WHERE table_name LIKE "mvd_%"
+        WHERE table_name LIKE "mvd\_%" escape '\'
     )
   LOOP
     EXECUTE immediate "DROP TABLE "||rec.table_name||" CASCADE CONSTRAINTS";
